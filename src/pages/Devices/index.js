a695bbdebc8e4e2fbf8675450825d5e169d5f768
giusepe01 <giusepe01@gmail.com>
2023-04-01 01:16:01 -0300

    Get no Firebase ao carregar a tela de dispositivos

--- src/pages/Devices/index.js
+++ src/pages/Devices/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import * as Animatable from 'react-native-animatable';
@@ -6,12 +6,23 @@ import * as Animatable from 'react-native-animatable';
 import { app } from '../../config/connectFirebase';
 import styles from '../Signin/style';
 import { useNavigation } from '@react-navigation/native';
+import { collection, getDocs, getFirestore } from 'firebase/firestore';
 
 export default function Init() {
+    const db = getFirestore(app);
+    const userCollectionRef = collection(db, "Devices" );
+
+    useEffect(() => {
+        const getDevices = async () => {
+            const data = await getDocs(userCollectionRef);
+            console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id })));
+        };
+        getDevices();
+    },[])
+
     const navigation = useNavigation();
     const connectedCallback = () => {
-        console.log('ABRIU A TELA');
-        } 
+        console.log('ABRIU A TELA');} 
 
     return (
         <View style ={styles.container}>
